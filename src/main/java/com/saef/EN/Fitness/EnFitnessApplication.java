c71bb12d6f50c9c4102764e27f0f0deb98eaa8e4
nicol <nicollasmauricio@outlook.com>
2024-10-20 00:20:12 -0300

    Commit All

---
+++ src/main/java/com/saef/EN/Fitness/EnFitnessApplication.java
@@ -0,0 +1,14 @@
+package com.saef.EN.Fitness;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnFitnessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnFitnessApplication.class, args);
+	}
+
+}
